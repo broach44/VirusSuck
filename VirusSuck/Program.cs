2e227ac8bc55724879956c3029e9b897231eac88
Crystal Broach <crysland@gmail.com>
2020-03-16 18:24:19 -0500

    Add project files.

---
+++ VirusSuck/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace VirusSuck
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var names = new List<string>();
+            names.Add("COVID-19");
+            names.Add("Ebola");
+            names.Add("Spanish Flu");
+            names.Add("Rabies");
+            names.Add("SARS");
+            names.Add("Stupidity");
+
+            names.Remove("Stupidity");
+        }
+    }
+}
